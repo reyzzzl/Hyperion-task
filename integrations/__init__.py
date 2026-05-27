ee86542128e79c27c3de6b78caabd8af9ae9fc5a
reyzzzl <ahmadrizkiafrizal1@gmail.com>
2026-05-27 18:52:55 +0700

    Create integrations/__init__.py

---
+++ integrations/__init__.py
@@ -0,0 +1,5 @@
+from .erp import ERPConnector, DummyERPConnector
+from .notion_erp import NotionERPConnector
+from .google_workspace import GoogleWorkspace
+from .microsoft365 import Microsoft365
+from .libreoffice import LibreOffice
